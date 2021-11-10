c88f7ad53f18c32dac9c90277449c418ab987390
abbv0 <44280015+abbv0@users.noreply.github.com>
2021-11-11 00:45:13 +0300

    Add files via upload

---
+++ lesson7/lesson7_1.py
@@ -0,0 +1,35 @@
+#Реализовать класс Matrix (матрица). Обеспечить
+#перегрузку конструктора класса (метод init()),
+#который должен принимать данные (список списков)
+#для формирования матрицы.Далее реализовать
+#перегрузку метода add() для реализации операции
+#сложения двух объектов класса Matrix (двух матриц).
+#Результатом сложения должна быть новая матрица.
+
+class Matrix:
+    def __init__(self, list_1, list_2):
+        self.list_1 = list_1
+        self.list_2 = list_2
+
+    def __add__(self):
+        matr = [[0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]]
+
+        for i in range(len(self.list_1)):
+            for j in range(len(self.list_2[i])):
+                matr[i][j] = self.list_1[i][j] + self.list_2[i][j]
+
+        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matr]))
+
+    def __str__(self):
+        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matr]))
+
+my_matrix = Matrix([[10, 1, 16],
+                    [14, 11, 16],
+                    [13, 27, 35]],
+                   [[23, 23, 29],
+                    [17, 41, 14],
+                    [19, 53, 7]])
+
+print(my_matrix.__add__())
---
+++ lesson7/lesson7_3.py
@@ -0,0 +1,34 @@
+class Cell:
+    def __init__(self, quantity):
+        self.quantity = int(quantity)
+
+    def __str__(self):
+        return f'Результат операции {self.quantity * "*"}'
+
+    def __add__(self, other):
+        return Cell(self.quantity + other.quantity)
+
+    def __sub__(self, other):
+        return Cell(int(self.quantity - other.quantity)) if (self.quantity - other.quantity) > 0 else print('Результат операции Отрицательное значение')
+
+    def __mul__(self, other):
+        return Cell(int(self.quantity * other.quantity))
+
+    def __truediv__(self, other):
+        return Cell(round(self.quantity // other.quantity)) if (self.quantity - other.quantity) > 0 else print('Результат операции Нехорошее деление')
+
+    def make_order(self, cells_in_row):
+        row = ''
+        for i in range(int(self.quantity / cells_in_row)):
+            row += f'{"*" * cells_in_row} \\n'
+        row += f'{"*" * (self.quantity % cells_in_row)}'
+        return row
+
+cells1 = Cell(4)
+cells2 = Cell(8)
+print(cells1)
+print(cells1 + cells2)
+print(cells2 - cells1)
+print(cells2.make_order(15))
+print(cells1.make_order(5))
+print(cells1 / cells2)
