9ac33abf60e12cc9b1badd64c37d42aae32f81f6
Pivovarchuk Andrey <44280015+abbv0@users.noreply.github.com>
2022-09-20 22:29:39 +0300

    Update and rename lesson5/lesson5_5.py to Работа с файлами/РасчётСуммыЧиселВФайле.py

---
+++ Работа с файлами/РасчётСуммыЧиселВФайле.py
@@ -0,0 +1,19 @@
+"""
+Создать (программно) текстовый файл, записать в него программно набор чисел,
+разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и
+выводить ее на экран.
+"""
+
+def summary():
+    try:
+        with open('file_5', 'w+') as file_obj:
+            line = input('Введите цифры через пробел - ')
+            file_obj.writelines(line)
+            my_numb = line.split()
+
+            print(sum(map(int, my_numb)))
+    except IOError:
+        print('Файловая ошибка')
+    except ValueError:
+        print('ValueErOrR')
+summary()
